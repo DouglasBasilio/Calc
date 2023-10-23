e24acaf6aad8a735bfb3c26589867c36cde2fe5d
basilio-viahub <douglas.basilio@via.com.br>
2023-10-23 15:04:56 -0300

    Add project files.

---
+++ Calc/Calculadora.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calc
+{
+    public class Calculadora
+    {
+        private List<string> lstHistoricoCalculos;
+
+        public Calculadora()
+        {
+            lstHistoricoCalculos = new List<string>();
+        }
+
+        // Método privado para adicionar uma entrada ao histórico
+        private void RegistrarHistorico(string operacao, int a, int b, int resultado, DateTime data)
+        {
+            lstHistoricoCalculos.Insert(0, $"{a} {operacao} {b} = {resultado} - Data: {data}");
+        }
+
+        // Método estático para somar dois números
+        public int Somar(int a, int b)
+        {
+            int resultado = a + b;
+            RegistrarHistorico("+", a, b, resultado, DateTime.Now);
+            return resultado;
+        }
+
+        // Método estático para subtrair dois números
+        public int Subtrair(int a, int b)
+        {
+            int resultado = a - b;
+            RegistrarHistorico("-", a, b, resultado, DateTime.Now);
+            return resultado;
+        }
+
+        // Método estático para multiplicar dois números
+        public int Multiplicar(int a, int b)
+        {
+            int resultado = a * b;
+            RegistrarHistorico("*", a, b, resultado, DateTime.Now);
+            return resultado;
+        }
+
+        // Método estático para dividir dois números
+        public int Dividir(int a, int b)
+        {
+            //if (b == 0)
+            //{
+            //    throw new ArgumentException("Não é possível dividir por zero.");
+            //}
+            int resultado = a / b;
+            RegistrarHistorico("/", a, b, resultado, DateTime.Now);
+            return resultado;
+        }
+
+        // Método para obter o histórico
+        public List<string> Historico()
+        {
+            // Limitar o histórico a 3 entradas
+            if (lstHistoricoCalculos.Count > 3)
+            {
+                lstHistoricoCalculos.RemoveRange(3, lstHistoricoCalculos.Count - 3);
+            }
+            return lstHistoricoCalculos;
+        }
+    }
+}
---
+++ TesteCalc/UnitTest1.cs
@@ -0,0 +1,88 @@
+using Calc;
+using System;
+using Xunit;
+
+namespace TesteCalc
+{
+    public class UnitTest1
+    {
+        public Calculadora construirClasse()
+        {
+            Calculadora calc = new Calculadora();
+
+            return calc;
+        }
+
+        [Theory]
+        [InlineData (1, 2, 3)]
+        [InlineData (4, 5, 9)]
+        public void TesteSomar(int val1, int val2, int resultado)
+        {
+            Calculadora calc = construirClasse();
+
+            int resultadoCalculadora = calc.Somar(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(6, 2, 4)]
+        [InlineData(6, 5, 1)]
+        public void TesteSubtrair(int val1, int val2, int resultado)
+        {
+            Calculadora calc = construirClasse();
+
+            int resultadoCalculadora = calc.Subtrair(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(3, 2, 6)]
+        [InlineData(4, 2, 8)]
+        public void TesteMultiplicar(int val1, int val2, int resultado)
+        {
+            Calculadora calc = construirClasse();
+
+            int resultadoCalculadora = calc.Multiplicar(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(6, 2, 3)]
+        [InlineData(5, 5, 1)]
+        public void TesteDividir(int val1, int val2, int resultado)
+        {
+            Calculadora calc = construirClasse();
+
+            int resultadoCalculadora = calc.Dividir(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Fact]
+        public void TestarDivisaoPorZero()
+        {
+            Calculadora calc = construirClasse();
+
+            Assert.Throws<DivideByZeroException>(() => calc.Dividir(5, 0));
+        }
+
+        [Fact]
+        public void TestarHistorico()
+        {
+            Calculadora calc = construirClasse();
+
+            calc.Somar(1, 2);
+            calc.Somar(2, 5);
+            calc.Somar(3, 6);
+            calc.Somar(4, 7);
+
+            var lista = calc.Historico();
+
+            Assert.NotEmpty(lista);
+            Assert.Equal(3, lista.Count);
+        }
+    }
+}
